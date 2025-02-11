d33fc120360c2335dd62d531b2d789b3845ae99b
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2025-02-11 14:28:13 -0300

    Aula 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
