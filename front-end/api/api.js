344c12b326f09caea6262fc3ffb61569a0700ab7
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2025-02-14 13:24:03 -0300

    Ajuste no process ENV

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-undef */
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3001/api";
+const URL = "https://spotify-replica.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
