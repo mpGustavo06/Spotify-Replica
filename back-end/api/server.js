8a14c935eecf420b41675794a48c4e8495b066f1
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2025-02-14 13:14:04 -0300

    Ajustes para o Deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,23 +1,32 @@
 import express from "express";
 import cors from "cors";
 import { database } from "./connect.js";
+import path from "path";
 
-const PORT = 3000;
+const __dirname = path.resolve();
+
+const PORT = 3001;
 const app = express();
 app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
-  response.json(await database.collection("artists").find({}).toArray());
+app.get("/api/artists", async (request, response) => {
+  response.send(await database.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
-  response.json(await database.collection("songs").find({}).toArray());
+app.get("/api/songs", async (request, response) => {
+  response.send(await database.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
